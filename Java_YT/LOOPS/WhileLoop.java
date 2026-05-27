20b847e6a2e76224572736a8bf00cb6e76af8eea
Utpala9 <utpaladutta69@gmail.com>
2026-05-27 12:59:31 +0530

    while loop

--- Java_YT/LOOPS/WhileLoop.java
+++ Java_YT/LOOPS/WhileLoop.java
@@ -1,5 +1,37 @@
 package LOOPS;
 
-public class WhileLoop {
+public class WhileLoop 
+{
+    /**
+     * @param args
+     */
+    public static void main(String a[])
+    {
+        int i = 1;
+        while(i<=5)
+        {
+            System.out.println("its Monday" + i);
+            System.out.println("its Tuesday" + i);
+            System.out.println("its Wednesday" + i);
+            System.out.println("its Thursday" + i);
+            System.out.println("its Friday" + i);
+            
+            
+            int j = 1;
+            while (j<=1)
+                {
+                    
+                    System.out.println("Yey irs saturday n its a holiday" + i);
+                    System.out.println("Yey irs sunday n its a holiday" + i);
+                    j++;
+                }
+
+            i++;
+        }
+
+        System.out.println("Ok Byeeee" + i);
+    }
+
+    
     
 }
