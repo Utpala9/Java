e3013cf6bceb06f41cf801df7f74ec375a9d6964
Utpala9 <utpaladutta69@gmail.com>
2026-05-27 13:31:05 +0530

    ForLoopEdite

--- Java_YT/LOOPS/ForLoop.java
+++ Java_YT/LOOPS/ForLoop.java
@@ -11,18 +11,18 @@ public class ForLoop {
 
         for(int i=1; i<=5; i++)         
         {
-            System.out.println("its Monday" + i);
-            System.out.println("its Tuesday" + i);
-            System.out.println("its Wednesday" + i);
-            System.out.println("its Thursday" + i);
-            System.out.println("its Friday" + i);
+            System.out.println("its Monday (work 9 to 5)" + " Week :" + i);
+            System.out.println("its Tuesday (work 9 to 5)" + " Week :" + i);
+            System.out.println("its Wednesday (work 9 to 5)" + " Week :" + i);
+            System.out.println("its Thursday (work 9 to 5)" + " Week :" + i);
+            System.out.println("its Friday (work 9 to 5)" + " Week :" + i);
             
             
             for (int j = 1; j <= 1; j++) //Nested for loop
                 {
                     
-                    System.out.println("Yey irs saturday n its a holiday" + i);
-                    System.out.println("Yey irs sunday n its a holiday" + i);
+                    System.out.println("Yey its saturday n its a holiday" + " Week :" + i);
+                    System.out.println("Yey its sunday n its a holiday" + " Week :" + i);
                     
                 }
         }
