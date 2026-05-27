6f6892c085e6fb2d8a7e9e70f522dd1977841bcc
Utpala9 <utpaladutta69@gmail.com>
2026-05-27 13:19:07 +0530

    Do while loop

--- Java_YT/LOOPS/DoWhileLoop.java
+++ Java_YT/LOOPS/DoWhileLoop.java
@@ -1,5 +1,16 @@
 package LOOPS;
 
 public class DoWhileLoop {
+
+    public static void main(String []args)
+    {
+        int i=5;
+        do
+        {
+            System.out.println(" Its a proof that the statement will run 1 time even the condition is false ");
+            i++;
+        }
+        while(i<=4);
+    }
     
 }
