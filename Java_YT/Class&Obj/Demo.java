f5c007583983b185d28648a019376d3c01fdf833
Utpala9 <utpaladutta69@gmail.com>
2026-06-08 19:10:18 +0530

    ClassObj

--- Java_YT/Class&Obj/Demo.java
+++ Java_YT/Class&Obj/Demo.java
@@ -1,11 +1,30 @@
 package Java_YT.Class&Obj;
-  
+
+class Calculator{
+
+    int a ; // class can also have variables, here we are declaring an integer variable 'a' which can be used within the class
+
+    public int add() //public : access; int : return type; add: method name (which will perform operation)
+
+    {
+        System.out.println("This is the add method");
+        return 0; //return type is int so we have to return an integer value, here we are returning 0 as a placeholder
+    }
+}
+
 public class Demo  //Class name: Demo (Class = Blueprint of Object)
 {
     public static void main (String[] args) 
     {
-        //Creating Object ;
+        int num1 = 4;  // int (premitive data type)
+        int num2 = 5;//Creating Object ;
 
+        //int result = num1 + num2 ; 
+        //System.out.println("The sum of num1 and num2 is: " + result);
+    
+        Calculator calc = new Calculator(); //( to cansume space its imp to creat obj)Creating an object of the Calculator class, 'calc' is the reference variable that holds the object
+        //virtual object created in heap memory and 'calc' is pointing to that object
+        calc.add();
     }
         
 }
