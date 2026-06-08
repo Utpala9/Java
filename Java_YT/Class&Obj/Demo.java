69d6cd820d5220406ea8a9e0cbc38c86bb3c7b53
Utpala9 <utpaladutta69@gmail.com>
2026-06-08 19:18:11 +0530

    demo

--- Java_YT/Class&Obj/Demo.java
+++ Java_YT/Class&Obj/Demo.java
@@ -1,4 +1,4 @@
-package Java_YT.Class&Obj;
+
 
 class Calculator{
 
@@ -17,14 +17,17 @@ public class Demo  //Class name: Demo (Class = Blueprint of Object)
     public static void main (String[] args) 
     {
         int num1 = 4;  // int (premitive data type)
-        int num2 = 5;//Creating Object ;
+        int num2 = 5; //Creating Object ;
 
         //int result = num1 + num2 ; 
         //System.out.println("The sum of num1 and num2 is: " + result);
     
         Calculator calc = new Calculator(); //( to cansume space its imp to creat obj)Creating an object of the Calculator class, 'calc' is the reference variable that holds the object
         //virtual object created in heap memory and 'calc' is pointing to that object
-        calc.add();
+        calc.add(); // calling the add method using the object 'calc'
     }
         
 }
+//To creat obj : 1st mention class name  > refral variable name > new keyword > class constructor
+//constructor is a special method that is used to initialize objects, it has the same name as the class and does not have a return type
+// method is a block of code that performs a specific task, it can have a return type and can be called to execute the code within it
