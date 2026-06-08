eb96f79e94481a7a5699bd8f916e22c03e217d41
Utpala9 <utpaladutta69@gmail.com>
2026-06-08 18:32:09 +0530

    TernaryOperator

--- Java_YT/IF_ELSE/TernaryOperator.java
+++ Java_YT/IF_ELSE/TernaryOperator.java
@@ -1,5 +1,20 @@
 package IF_ELSE;
 
-public class TernaryOperator {
+public class TernaryOperator
+ {
+    public static void main(String args[]){
+        int n = 5;
+        int result =0;
+        //if(n%2==0);
+            // result = 10;
+        // else 
+            // result = 20;
+
+        //  ? = if and : = else 
+
+            result = n%2==0 ? 10: 20 ;
+            System.out.println(result);
+
+    }
     
 }
